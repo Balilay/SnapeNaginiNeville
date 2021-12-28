68114f9eda9a76c9ccdb92d97aa6357e558cd9f7
Balilay <94835480+Balilay@users.noreply.github.com>
2021-12-28 16:00:28 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,24 +1,25 @@
 import random
 
 def play():
-    user = input("Play your move: Snape, Neville, or Nagini\n")
+    user = input("Play your move: Snape, Neville, or Nagini? \n")
     computer = random.choice(['Snape', 'Neville', 'Nagini'])
 
     if user == computer:
         return 'It\'s a tie'
       
-def is_win(player, opponent):
+      
+    # "Neville" > "Nagini", "Nagini" > "Snape", "Snape" > "Neville"
+    if is_win(user, computer):
+        return 'You won!'
+    
+def is_win(user, computer):
     # return true if player wins
     # '"Neville" > "Nagini", "Nagini" > "Snape", "Snape" > "Neville"
     if (player == 'Neville' and opponent == 'Nagini') or (player == 'Nagini' and opponent == 'Snape') \
         or (player == 'Snape' and opponent == 'Neville'):
         return True
-      
-      
-    # "Neville" > "Nagini", "Nagini" > "Snape", "Snape" > "Neville"
-    if is_win(user, computer):
-        return 'You won!'
-
+    
+    else:
     return 'You lost!'
 
 print(play())
