cf7d7f5eb06334199d03fc4d9897b86637aae6b3
Balilay <94835480+Balilay@users.noreply.github.com>
2021-12-28 16:36:34 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,23 +1,28 @@
 import random
 
-def play():
-    user = input("Play your move: Snape, Neville, or Nagini? \n")
-    computer = random.choice(['Snape', 'Neville', 'Nagini'])
-    
-def is_win(user, computer):
-    # return true if player wins
-    # '"Neville" > "Nagini", "Nagini" > "Snape", "Snape" > "Neville"
-    if (user == 'Neville' and computer == 'Nagini') or (user == 'Nagini' and computer == 'Snape') \
-        or (user == 'Snape' and computer == 'Neville'):
-        return True
+user_action = input("Play your move! Snape, Nagini or Neville? ")
 
-    if user == computer:
-        return "It's a tie"
-      
-    elif is_win(user, computer):
-        return "You won!"
-    
-    else:
-    return 'You lost!'
+possible_actions = ["Snape", "Nagini", "Neville"]
+computer_action = random.choice(possible_actions)
+print("You chose " + str(user_action) + ", computer chose " + str(computer_action) + "\n") 
 
-print(play())
+if user_action == computer_action:
+    print("Both players selected " + (user_move) + ". It's a tie!"
+elif user_action == "Snape":
+          if computer_action == "Neville":
+            print("Snape gives detention to Neville! You win!")
+          else:
+            print("Nagini eats Snape! You lose!")
+elif user_action == "Nagini":
+          if computer_action == "Snape":
+            print("Nagini eats Snape! You win!")
+          else: 
+            print("Neville beheads Nagini! You lose!")
+elif user_action == "Neville":
+          if computer_action == "Snape":
+            print("Snape gives detention to Neville! You lose!")
+          else: 
+            print("Neville beheads Nagini! You win!")
+          
+     
+
