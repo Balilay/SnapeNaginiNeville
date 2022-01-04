3ba798b76110f56c63ed6dffe3ce7d637a24c1a6
Balilay <94835480+Balilay@users.noreply.github.com>
2022-01-04 10:24:07 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,29 +1,32 @@
-import random
+from random import randint
 
-user_action = input("Play your move! Snape, Nagini or Neville? ")
+player_options = ["Snape", "Nagini", "Neville"]
 
-possible_actions = ["Snape", "Nagini", "Neville"]
-computer_action = random.choice(possible_actions)
-    print("You chose " + (user_action) + ", computer chose " + (computer_action)) 
+computer = player_options[randint(0,2)]
 
-    if user_action == computer_action:
-        print("Both players selected " + (user_move) + ". It's a tie!"
-              
-    elif user_action == "Snape":
-          if computer_action == "Neville":
-            print("Snape gives detention to Neville! You win!")
-          else:
-            print("Nagini eats Snape! You lose!")
-    elif user_action == "Nagini":
-          if computer_action == "Snape":
-            print("Nagini eats Snape! You win!")
-          else: 
-            print("Neville beheads Nagini! You lose!")
-    elif user_action == "Neville":
-          if computer_action == "Snape":
-            print("Snape gives detention to Neville! You lose!")
-          else: 
-            print("Neville beheads Nagini! You win!")
-          
-     
+player = False
 
+while player == False:
+    player = input("Make your choice! Snape, Nagini or Neville? ")
+    if player == computer:
+        print("It's a tie!")
+    elif player == "Snape":
+        if computer == "Nagini":
+            print("You lose!", computer, "eats ", player)
+        else:
+            print("You win!", player, "gives detention to ", computer)
+    elif player == "Nagini":
+        if computer == "Neville":
+            print("You lose!", computer, "beheads", player)
+        else:
+            print("You win!", player, "eats", computer)
+    elif player == "Neville":
+        if computer == "Snape":
+            print("You lose...", computer, "gives detention to", player)
+        else:
+            print("You win!", player, "beheads", computer)
+    else:
+        print("That's not a valid play. Check your spelling!")
+
+    player = False
+    computer = player_options[randint(0,2)]
